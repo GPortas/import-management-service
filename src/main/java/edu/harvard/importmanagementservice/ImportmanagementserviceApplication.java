29958a419466aa4386cb2c77f78f30b31dfd7b9d
GPortas <hey@gportas.me>
2022-03-01 11:47:55 +0000

    Added: Spring Boot project initialization

---
+++ src/main/java/edu/harvard/importmanagementservice/ImportmanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.harvard.importmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportmanagementserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportmanagementserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/harvard/importmanagementservice/ImportmanagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.harvard.importmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportmanagementserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
