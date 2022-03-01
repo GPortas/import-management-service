f6f52fe01a4dfc80e2764383a6c3d57c7c2d8775
GPortas <hey@gportas.me>
2022-03-01 16:33:43 +0000

    Added: health check controller

--- src/main/java/edu/harvard/importmanagementservice/ImportManagementServiceApplication.java
+++ src/main/java/edu/harvard/importmanagementservice/ImportManagementServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImportmanagementserviceApplication {
+public class ImportManagementServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImportmanagementserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ImportManagementServiceApplication.class, args);
+    }
 
 }
---
+++ src/main/java/edu/harvard/importmanagementservice/health/application/HealthController.java
@@ -0,0 +1,13 @@
+package edu.harvard.importmanagementservice.health.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Ok";
+    }
+}
